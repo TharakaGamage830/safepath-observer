22e7d9c21d3419798d12ae324c089919e3dd9d3e
Gimhani Ahinsa <157673666+gimhaniahinsa2001@users.noreply.github.com>
2025-08-04 14:11:09 +0530

    update apiStudent

--- app/api/api.php
+++ app/api/api.php
@@ -1,5 +1,9 @@
 <?php
-require_once __DIR__ . '/../../config/config.php';
+ob_start();
+require_once __DIR__ . '/../../../config/config.php';
+require_once __DIR__ . '/../../api/api.php';
+?>
+<?php
 // Get total number of students
 $totalStudentsQuery = "SELECT COUNT(*) as total FROM students";
 $totalStudentsResult = $conn->query($totalStudentsQuery);
